aebc80a4b1b23d607fa8503b5d27fd9edad2ae4b
Eric Wohnlich <wohnlice@imsweb.com>
2012-04-24 15:09:38 +0000

    unzipping support

---
+++ ims/zip/browser/unzipper.py
@@ -0,0 +1,69 @@
+import zipfile, os
+from zope import component
+from Products.CMFCore.interfaces import ISiteRoot
+from StringIO import StringIO
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory
+
+class Unzipper(BrowserView):
+
+  def __init__(self,context,request):
+    self.context=context
+    self.request=request
+
+  def __call__(self):
+    form = self.request.form
+    if 'form.submitted' in form:
+      zipf = self.request.form.get('file','')
+      folder = self.request.form.get('folder','')
+      return self.unzip(zipf,folder)
+    return self.index()
+
+  def unzip(self, zipf, init):
+    portal = component.getUtility(ISiteRoot)
+    mimereg = getToolByName(portal,'mimetypes_registry')
+    zipper = zipfile.ZipFile(zipf, 'r')
+
+    for name in zipper.namelist():
+      path,id = os.path.split(name)
+      stream = zipper.read(name)
+      curr = self.context[init]
+      for folder in [f for f in path.split('/') if f]:
+        try:
+          curr = curr[folder]
+        except KeyError:
+          curr.invokeFactory('Folder',folder)
+          curr = curr[folder]
+          curr.setTitle(folder)
+          curr.reindexObject()
+      mimetype = mimereg.lookupExtension(id)
+      factory = None
+      if [m for m in mimetype.mimetypes if 'image/' in m]:
+        factory = self.createImage
+      elif 'text/html' == mimetype:
+        factory = self.createDocument
+      else:
+        factory = self.createFile
+      factory(curr, id, stream)
+      
+      self.context.plone_utils.addPortalMessage(PloneMessageFactory(u'Zip file imported'))
+    return self.context[init]()
+      
+  def createFile(self, parent, id, stream):
+    parent.invokeFactory('File',id)
+    ob=parent[id]
+    ob.setFile(stream)
+    ob.reindexObject()
+      
+  def createImage(self, parent, id, stream):
+    parent.invokeFactory('Image',id)
+    ob=parent[id]
+    ob.setImage(stream)
+    ob.reindexObject()
+      
+  def createDocument(self, parent, id, stream):      
+    parent.invokeFactory('Document',id)
+    ob=parent[id]
+    ob.setText(stream,mimetype="text/html")
+    ob.reindexObject()
--- ims/zip/permissions.py
+++ ims/zip/permissions.py
@@ -2,3 +2,5 @@ from Products.CMFCore.permissions import setDefaultRoles
 
 CanZip='ims.zip: can zip'
 setDefaultRoles(CanZip,('Manager',))
+CanZip='ims.zip: can unzip'
+setDefaultRoles(CanZip,('Manager',))
