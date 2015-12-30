22bcc046f7478be3f66ea6b587623fd4ce71194d
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-12-30 17:33:49 -0500

    convert to dexterity

--- ims/zip/adapters.py
+++ ims/zip/adapters.py
@@ -15,34 +15,27 @@ class AdapterBase(object):
       return ''
 
 class FileZip(AdapterBase):
-    """ for ATFile type """
+    """ for File type """
     def zippable(self):
-      return self.context.get_data()
+      primary_field = IPrimaryFieldInfo(self.context)
+      return primary_field.value.data
     def extension(self):
       id = self.context.getId()
       primary_field = IPrimaryFieldInfo(self.context)
-      import pdb; pdb.set_trace()
-      fn = self.context.file.filename or id
+      fn = primary_field.value.filename or id
       return id.split('.')[-1] != fn.split('.')[-1] and '.'+fn.split('.')[-1] or ''
 
-class ImageZip(AdapterBase):
-    """ for ATImage type """
-    def zippable(self):
-      if IATBlobImage.providedBy(self.context):
-          primary_field = IPrimaryFieldInfo(self.context)
-          return primary_field.value
-          #return str(self.context.getPrimaryField().getAccessor(self.context)())
-      else:
-          return self.context.file.value
+class ImageZip(FileZip):
+    """ for Image type """
 
 class DocumentZip(AdapterBase):
-    """ for ATDocument type"""
+    """ for Document type"""
     def zippable(self):
       template = '<html><body>%(header)s%(description)s%(text)s</body></html>'
 
       header = self.context.title and '<h1>%s</h1>' % self.context.title or ''
-      description = self.context.Description() and '<p class="description">%s</p>' % self.context.description or ''
-      text = self.context.text
+      description = self.context.description and '<p class="description">%s</p>' % self.context.description or ''
+      text = self.context.text.raw
 
       html = template % {'header':header,'description':description,'text':text}
       return html
